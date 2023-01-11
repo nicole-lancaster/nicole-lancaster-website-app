eee89fe05e4cb2a15965ec227c4b26fe648def9c
Nicole <lancasternj@gmail.com>
2023-01-11 14:38:51 +0000

    Installed prettier as a dev dependency and ran for first time

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -2,16 +2,20 @@
 //any function in React - put export at the start
 export const HomePage = () => {
   //if using HTML with TS, need to put in ( ) after the return keyword
-  return (<div>
-        <h1>Welcome to my Home Page!</h1>
-        <h2>This page was made by Nicole Lancaster using the following technologies:</h2>
-        <p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript/TypeScript</li>
-                <li>React Hooks (functional paradigm)</li>
-            </ul>
-        </p>
-    </div>) 
-}
+  return (
+    <div>
+      <h1>Welcome to my Home Page!</h1>
+      <h2>
+        This page was made by Nicole Lancaster using the following technologies:
+      </h2>
+      <p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript/TypeScript</li>
+          <li>React Hooks (functional paradigm)</li>
+        </ul>
+      </p>
+    </div>
+  );
+};
